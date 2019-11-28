b8c26898c68efaacf178303f54fc32f66b5a485a
David Milligan <davidgeraldmilligan@gmail.com>
2019-11-28 11:05:18 +0000

    renamed

--- src/com/davemilligan/interview/matrix/PrintReverseDiagonal.java
+++ src/com/davemilligan/interview/matrix/PrintReverseDiagonal.java
@@ -1,30 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Solution {
+public class PrintReverseDiagonal {
 
-    public static void main(String[] args) throws IOException {
+    static List<List<Integer>> matrix = new ArrayList<>();
+
+    static {
         List<Integer> one =     Arrays.asList(6 , 3,  1,  0);
         List<Integer> two =     Arrays.asList(10, 7,  4,  2);
         List<Integer> three =   Arrays.asList(13, 11, 8,  5);
         List<Integer> four =    Arrays.asList(15, 14, 12, 9);
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(one);
-        list.add(two);
-        list.add(three);
-        list.add(four);
-        reverseDiag(list);
+
+        matrix.add(one);
+        matrix.add(two);
+        matrix.add(three);
+        matrix.add(four);
     }
 
-    public static void reverseDiag(List<List<Integer>> list) {
-        int n = list.size(), startCol = n, startRow = 0;
+    public static void main(String[] args) throws IOException {
+        reverseDiagonalMatrix();
+    }
+
+    public static void reverseDiagonalMatrix() {
+        int n = matrix.size(), startCol = n, startRow = 0;
+
         while (startRow < n) {
             while (startCol >= 0) {
                 int x = startRow, y = startCol--;
                 while (x < n && y < n) {
-                    System.out.println(list.get(x++).get(y++));
+                    System.out.println(matrix.get(x++).get(y++));
                 }
             }
+            startCol = 0;
             startRow++;
         }
     }
