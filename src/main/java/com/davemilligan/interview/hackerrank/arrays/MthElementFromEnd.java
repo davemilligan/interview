4072f868c1c4ff7efdf072ea1fd8cd754465d8cc
davemilligan <davidgeraldmilligan@gmail.com>
2019-12-06 10:53:40 +0000

    Adding trading example, streams, dates, map reduce

---
+++ src/main/java/com/davemilligan/interview/hackerrank/arrays/MthElementFromEnd.java
@@ -0,0 +1,20 @@
+package com.davemilligan.interview.hackerrank.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MthElementFromEnd {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader sc = new BufferedReader(new InputStreamReader(System.in))) {
+            int m = Integer.parseInt(sc.readLine());
+            String[] a = sc.readLine().split("\\s+");
+            int len = a.length;
+            if (m >= len)
+                System.out.println("NIL");
+            else
+                System.out.println(a[len - m]);
+        }
+    }
+}
---
+++ src/main/java/com/davemilligan/interview/sales/Trading.java
@@ -0,0 +1,100 @@
+package com.davemilligan.interview.sales;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Trading {
+
+    public static List<Product> getProductsSellingOver(
+            List<Sale> sales,
+            LocalDate startDate,
+            LocalDate endDate,
+            int boundary) {
+        final Map<Integer, Integer> salesPerProduct = new HashMap<>();
+
+        getSalesBetweenDates(
+                sales,
+                startDate,
+                endDate).forEach(s -> {
+            int currentTotal = (salesPerProduct.containsKey(s.pid)) ? salesPerProduct.get(s.pid) : 0;
+            salesPerProduct.put(s.pid, currentTotal + s.volume);
+        });
+
+        final List<Integer> pids = new ArrayList<>();
+        salesPerProduct.keySet().stream().forEach(pid -> {
+            if (salesPerProduct.get(pid) > 200) {
+                pids.add(pid);
+            }
+        });
+        return pids
+                .stream()
+                .map(pid -> getProduct(pid))
+                .collect(Collectors.toList());
+    }
+
+    public static int getTotalsBetweenDates(
+            List<Sale> sales,
+            LocalDate startDate,
+            LocalDate endDate) {
+        return getSalesBetweenDates(sales, startDate, endDate)
+                .stream()
+                .map(s -> s.volume)
+                .reduce(0, (total, volume) -> {
+                    return total += volume;
+                });
+    }
+
+    public static int getSalesTotalsOnDate(List<Sale> sales, LocalDate date) {
+        return getSalesOnDate(sales, date)
+                .stream()
+                .map(s -> s.volume)
+                .reduce(0, (total, volume) -> {
+                    return total += volume;
+                });
+    }
+
+    public static List<Sale> getSalesBetweenDates(
+            List<Sale> sales,
+            LocalDate startDate,
+            LocalDate endDate) {
+        return sales
+                .stream()
+                .filter(s-> s.date.isAfter(startDate) && s.date.isBefore(endDate))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Sale> getSalesOnDate(List<Sale> sales, LocalDate date) {
+        return sales
+                .stream()
+                .filter(s-> s.date.equals(date))
+                .collect(Collectors.toList());
+    }
+
+    public static int getSales(List<Sale> sales) {
+        return sales.stream().map(s -> s.volume).reduce(0, (total, volume) -> {
+            return total += volume;
+        });
+    }
+
+    //  Placeholder
+    public static Product getProduct(int pid) {
+        return new Product();
+    }
+}
+
+
+class Sale {
+    int pid;
+    int volume;
+    LocalDate date;
+}
+
+class Product {
+    int id;
+    double price;
+    String name;
+}
